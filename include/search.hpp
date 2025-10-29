d8dbf7726fdec8949c833f9bc85d35cb202aa07f
RCube-gh <icegame1118@gmail.com>
2025-10-29 14:19:36 +0900

    commit

--- include/search.hpp
+++ include/search.hpp
@@ -9,6 +9,7 @@ public:
 	explicit Searcher(const std::vector<Entry>& entries);
 	std::vector<Entry> search(const std::string& query)const;
 	std::string bestMatch(const std::string& query)const;
+	std::vector<std::string> getCompletions(const std::string& query, int limit = 10)const;
 
 private:
 	const std::vector<Entry>& entries;
